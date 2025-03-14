4c5ac5954fba5c4260d3461513f198e5df725e2f
KanyantaM <kanyanta.1makasa@gmail.com>
2025-03-15 01:30:08 +0200

    adding more groupings

--- lib/src/helper/grouping.dart
+++ lib/src/helper/grouping.dart
@@ -1,6 +1,6 @@
-//** getting all the courses */
 import 'package:reviza_courses/reviza_courses.dart';
 
+//** getting all the courses */
 List<String> getAllCourses() {
   List<String> courses = [];
 
@@ -13,4 +13,34 @@ List<String> getAllCourses() {
   });
 
   return courses;
+}
+
+//** Getting all universities */
+List<String> getAllUniversities() {
+  return data.keys.toList();
+}
+
+//** Getting all schools belonging to a university */
+List<String> getSchools(String university) {
+  return data[university]?.keys.toList() ?? [];
+}
+
+//** Getting all departments belonging to a school */
+List<String> getDepartments(String university, String school) {
+  return data[university]?[school]?.keys.toList() ?? [];
+}
+
+//** Getting all years belonging to a department */
+List<String> getYears(String university, String school, String department) {
+  return data[university]?[school]?[department]?.keys.toList() ?? [];
+}
+
+//** Getting all the courses for the given year */
+List<String> getCoursesForYear(
+  String university,
+  String school,
+  String department,
+  String year,
+) {
+  return data[university]?[school]?[department]?[year] ?? [];
 }
